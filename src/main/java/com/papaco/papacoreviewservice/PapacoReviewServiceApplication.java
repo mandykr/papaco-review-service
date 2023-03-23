0f3531f5a4de15ed70c14a47b49804f9c8e3bfaa
mandykr <hongseokii92@gmail.com>
2023-03-23 14:45:33 +0900

    Initial commit

---
+++ src/main/java/com/papaco/papacoreviewservice/PapacoReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.papaco.papacoreviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapacoReviewServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PapacoReviewServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/papaco/papacoreviewservice/PapacoReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papaco.papacoreviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapacoReviewServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
